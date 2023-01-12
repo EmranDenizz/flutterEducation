e654472f4372d23c5074cd6a12ab0e01cd512511
Emran Deniz <63171566+EmranDenizz@users.noreply.github.com>
2023-01-12 23:26:01 +0300

    updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,57 @@
+import 'package:flutter/material.dart';
 
+import './question.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  var questionList = [
+    'What is favorite color?',
+    'What is favorite animal?',
+  ];
+
+  void changeQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter excersis'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Question(
+              questionList[questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: (changeQuestion),
+              child: Text('Button1'),
+            ),
+            ElevatedButton(
+              onPressed: (changeQuestion),
+              child: Text('Button2'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
